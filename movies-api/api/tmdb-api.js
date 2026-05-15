6ae9287a0990191fc1a52545dee55d0e7ab4bd84
Mariia Popovych <20107971@setu.ie>
2026-05-15 22:30:55 +0100

    2. getUpcomingMovies

--- movies-api/api/tmdb-api.js
+++ movies-api/api/tmdb-api.js
@@ -11,3 +11,15 @@ export const getMovie = async (id) => {
 
     return await response.json();
 };
+
+export const getUpcomingMovies = async () => {
+    const response = await fetch(
+        `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.TMDB_KEY}&language=en-US&page=1`
+    );
+
+    if (!response.ok) {
+        throw new Error(response.json().message);
+    }
+
+    return await response.json();
+};
